ce92dd8586aec94feb00198faeabd99745b480a1
Laurenda Edorh <laurenda98@gmail.com>
2024-10-10 02:15:07 +0200

    Commit de la generation de mon premier build Angualar

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  standalone: true,
+  imports: [],
+  templateUrl: './face-snap.component.html',
+  styleUrl: './face-snap.component.scss'
+})
+export class FaceSnapComponent implements OnInit {
+
+  title!:string;
+  description!:string;
+  createdAt!:Date;
+  snaps!:number;
+  imageUrl!:string;
+  snapButtonText!:string;
+  userHasSnapped!:boolean;
+
+
+ngOnInit(): void {
+   this.title ='Archinald et des copains';
+   this.description="ma premiere snaps";
+   this.createdAt= new Date();
+   this.snaps = 2;
+   this.imageUrl="https://cdn.pixabay.com/photo/2022/09/27/19/46/ai-generated-7483596_960_720.jpg"
+   this.snapButtonText = "oh snap";
+   this.userHasSnapped = false;
+  }
+  
+
+   OnSnap():void
+   {
+    if(this.userHasSnapped)
+    {
+     this.unSnap();
+      
+    }
+    else{
+   this.snap();
+    }
+   
+   }
+unSnap():void
+{
+  this.snaps --;
+  this.snapButtonText="Oh snap!"
+  this.userHasSnapped= false;
+
+
+}
+
+snap():void{
+     this.snaps ++;
+      this.snapButtonText = 'Oops, unSnap!';
+      this.userHasSnapped= true;
+
+}
+   
+
+}
