92fbdc11528e657095d0bc8f4fb6254a5dc70ac4
Laurenda Edorh <laurenda98@gmail.com>
2024-10-10 02:27:07 +0200

    Creation d'une classe pour gerer les elements

--- src/app/models/face-snap.ts
+++ src/app/models/face-snap.ts
@@ -1,5 +1,16 @@
 export class FaceSnap{
 
-    
+    constructor(public title: string, 
+               public description:string,
+                public createdAt:Date,
+                public snaps:number,
+                public imageUrl:string)
+    {
+        
+
+    }
+
+ 
+
 
 }
