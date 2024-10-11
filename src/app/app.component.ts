26c4955953ac071d154c727ab884c842efc5f488
Laurenda Edorh <laurenda98@gmail.com>
2024-10-11 13:09:17 +0200

    utilisation de @foreach en angular pour afficher les snaps ou photo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,38 +12,51 @@ import { FaceSnap } from './models/face-snap';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-
+faceSnaps!:FaceSnap[];
   mySnap!:FaceSnap;
   mySnap1!:FaceSnap;
   mySnap2!:FaceSnap;
 
   ngOnInit(): void {
-      this.mySnap = new FaceSnap(
-      'Archibald',
-      'MOn meilleur amie depuis toujours',
-      'https://cdn.pixabay.com/photo/2022/09/27/19/46/ai-generated-7483596_960_720.jpg',
-      new Date(),
-      10
 
-     );
-     this.mySnap1 = new FaceSnap(
-      'Laurenda un bon repas',
-      'MOn meilleur amie depuis toujours',
-      'https://cdn.pixabay.com/photo/2022/09/27/19/46/ai-generated-7483596_960_720.jpg',
-      new Date(),
-      5
+    this.faceSnaps=[
+      new FaceSnap(
+        'Archibald',
+        'MOn meilleur amie depuis toujours',
+        'https://cdn.pixabay.com/photo/2022/09/27/19/46/ai-generated-7483596_960_720.jpg',
+        new Date(),
+        10
+  
+       ),
+       new FaceSnap(
+        'Laurenda un bon repas',
+        'MOn meilleur amie depuis toujours',
+        'https://cdn.pixabay.com/photo/2022/09/27/19/46/ai-generated-7483596_960_720.jpg',
+        new Date(),
+        5
+  
+       ),
+       new FaceSnap(
+        'koukouvi trhee montain',
+        'MOn meilleur amie depuis toujours',
+        'https://cdn.pixabay.com/photo/2022/09/27/19/46/ai-generated-7483596_960_720.jpg',
+        new Date(),
+        1
+  
+       ),
+       new FaceSnap(
+        'late trhee montain',
+        'MOn meilleurs toujours',
+        'https://cdn.pixabay.com/photo/2022/09/27/19/46/ai-generated-7483596_960_720.jpg',
+        new Date(),
+        1
+  
+       )
 
-     );
-     this.mySnap2 = new FaceSnap(
-      'koukouvi trhee montain',
-      'MOn meilleur amie depuis toujours',
-      'https://cdn.pixabay.com/photo/2022/09/27/19/46/ai-generated-7483596_960_720.jpg',
-      new Date(),
-      1
 
-     );
-
-     this.mySnap1.setLocation("25 rue de ma marne");
+    ];
+      
+     this.faceSnaps[1].setLocation("25 rue de ma marne");
   }
  
 }
